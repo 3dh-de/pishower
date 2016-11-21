90b2043ac68db30caf2da4585d9de075bc89615e
3dh-de <3dh@3dh.de>
2016-11-21 01:39:56 +0100

    added new method writeEntry()

--- dbconnection.py
+++ dbconnection.py
@@ -7,7 +7,7 @@ import logging
 # set up logging to file - see previous section for more details
 logging.basicConfig(
     level=logging.DEBUG, datefmt='%d.%m. %H:%M:%S',
-    format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s')
+    format='%(asctime)s %(levelname)-8s %(message)s')
 
 logger = logging.getLogger()
 
@@ -62,7 +62,8 @@ class DBConnection:
         sqls = []
         sqls.append("CREATE TABLE {0} ({1})".format(tableName, fieldStr))
         if primaryKey and not primaryKey.isspace():
-            sqls.append("CREATE UNIQUE INDEX index_{0}_{1} ON {0}({1})".format(tableName, primaryKey))
+            sqls.append("CREATE UNIQUE INDEX index_{0}_{1} "
+                        "ON {0}({1})".format(tableName, primaryKey))
         retval = self._exec_queries(sqls)
         return True if retval is not None else False
 
@@ -99,6 +100,52 @@ class DBConnection:
         retval = self._exec_queries(sqls)
         return True if len(retval) and retval[0] is not None else False
 
+    def writeEntry(self, tableName, primaryKey, values={}):
+        """ Insert or update a table entry with given values
+
+        tableName  - name of target table for insert/update of entry
+        primaryKey - name of table primary key or selector column
+        values     - dictionary with field:value pairs of entry to write
+
+        Return True on success, False on any error
+        """
+        if tableName is None:
+            logger.error('Invalid table name given!')
+            return False
+
+        if primaryKey is None:
+            logger.error('Invalid table entry primary key given!')
+            return False
+
+        if values is False or primaryKey not in values:
+            logger.error('Invalid table entry primary key given!')
+            return False
+
+        id = values[primaryKey]
+
+        if self.tableExists(tableName) is False:
+            logger.warning(
+                'Entry cannot be written! '
+                'Table does not exist!'.format(tableName))
+            return False
+
+        sqls = ["SELECT * FROM {0} WHERE {1} "
+                "= '{2}'".format(tableName, primaryKey, id)]
+        retval = self._exec_queries(sqls)
+        if len(retval) and retval[0] is not None:
+            logger.debug('Updating entry {0}="{1}"'.format(primaryKey, id))
+            sqls = ["UPDATE {0} SET {1} WHERE {2} = '{3}'".format(tableName, 
+                    ', '.join("%s='%s'" % (k, v) for k, v in values.items()), 
+                    primaryKey, id)]
+        else:
+            logger.debug('Inserting new entry {0}="{1}"'.format(primaryKey, id))
+            sqls = ["INSERT INTO {0} ({1}) VALUES ({2})".format(tableName, 
+                    ', '.join("{0}".format(k) for k in values.keys()), 
+                    ', '.join("'{0}'".format(values[k]) for k in values.keys()))]
+
+        retval = self._exec_queries(sqls)
+        return True if retval is not None else False
+
     def _exec_queries(self, sqlQueries=[]):
         """ Execute given SQL statements
 
@@ -119,9 +166,10 @@ class DBConnection:
                     sql = query
                     for row in cursor.execute(sql):
                         results.append(row)
-                        # logger.debug(
-                        #    'added query "{0}" results "{1}" '
-                        #    'to return value'.format(sql, row))
+                        if 0:
+                            logger.debug(
+                                'added query "{0}" results "{1}" '
+                                'to return value'.format(sql, row))
                     logger.debug('executed query "{0}"'.format(sql))
         except sqlite3.OperationalError as err:
             logger.error("{0}! query: {1}".format(err, sql))
@@ -138,4 +186,5 @@ if __name__ == "__main__":
         logger.error('check for existing table failed!')
     db.createTable('user', ['id INTEGER', 'name', 'firstname', 'age INTEGER'])
 
-
+    db.writeEntry('user', 'id', {'id': 2, 'name': 'Wurst', 'firstname': 'Hans', 'age': 24})
+    db.writeEntry('user', 'id', {'id': 2, 'name': 'Wurst', 'firstname': 'Hänschen', 'age': 30})
