ab360c2a094f22aee3a403179ca4896638de7b0a
3dh-de <3dh@3dh.de>
2016-11-30 14:49:48 +0100

    added Flask example for webui, added INSTALL commands for python packages

---
+++ tests/testwebui.py
@@ -0,0 +1,20 @@
+import os
+import flaskr
+import unittest
+import tempfile
+
+class WebUITestCase(unittest.TestCase):
+    """ Basic unit test for web user interface in webui.py """
+
+    def setUp(self):
+        self.db_fd, flaskr.app.config['DATABASE'] = tempfile.mkstemp()
+        self.app = flaskr.app.test_client()
+        flaskr.init_db()
+
+    def tearDown(self):
+        os.close(self.db_fd)
+        os.unlink(flaskr.app.config['DATABASE'])
+
+    def test_empty_db(self):
+        rv = self.app.get('/')
+        assert b'No entries here so far' in rv.data
