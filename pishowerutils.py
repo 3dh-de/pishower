6dbd25377023f21981280c2b051def174982d5c4
3dh-de <3dh@3dh.de>
2017-01-02 01:55:36 +0100

    added global utils module for logging etc.

---
+++ pishowerutils.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import logging
+
+# set up logging to file
+logging.basicConfig(
+        level=logging.DEBUG,
+        datefmt='%d.%m. %H:%M:%S',
+        format='%(asctime)s %(levelname)-8s %(message)s')
+
+logger = logging.getLogger()
+
