4f83abe5f767e37c79dcab014cc339b910d1d64d
3dh-de <3dh@3dh.de>
2017-01-04 02:20:06 +0100

    new control class for HD44780 I2C LCD displays

---
+++ lcdcontrol.py
@@ -0,0 +1,96 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+import os
+import sys
+import time
+from pishowerutils import logger
+
+try:
+    scriptPath = os.path.realpath(os.path.dirname(sys.argv[0]))
+    os.chdir(scriptPath)
+    sys.path.append("./hd44780")
+    import lcddriver
+except RuntimeError:
+    logger.error('Error importing hd44780 lcddriver!')
+    quit(-1)
+
+
+class LcdControl:
+    """ Easy output to HD44780 based LCD displays """
+    __outputDevice = None
+    __deviceOpenRetries = 0
+
+    def __init__(self):
+        """ Init display interface """
+        self.open()
+
+    def __del__(self):
+        """ Clear display interface """
+        self.close()
+
+    def open(self):
+        """ Open connection to LCD display """
+        try:
+            self.__outputDevice = lcddriver.lcd()
+            if self.__outputDevice is None:
+                raise RuntimeWarning
+            self.__outputDevice.lcd_clear()
+            time.sleep(1.0)  # prevent timing issues
+        except:
+            if self.__deviceOpenRetries < 1:
+                logger.error('Unable to open HD44780 LCD display!')
+            self.__outputDevice = None
+            self.__deviceOpenRetries += 1
+
+    def close(self):
+        """ Close the input device and wait 1 second """
+        try:
+            if self.__outputDevice is not None:
+                del self.__outputDevice
+                self.__outputDevice = None
+                logger.debug('lcd device closed')
+                time.sleep(1.0)  # prevent timing issues
+        except:
+            logger.warning('Error while closing lcd device!')
+
+    def isOpen(self):
+        """ Returns True, if connection to LCD device is open """
+        return self.__outputDevice is not None
+
+    def clear(self):
+        """ Clears all text from LCD """
+        try:
+            if self.__outputDevice is not None:
+                self.__outputDevice.lcd_clear()
+        except:
+            logger.error('Clearing lcd display failed!')
+            self.close()
+
+    def show(self, text, line=1):
+        """ Displays given text on LCD at given line (1..n) """
+        try:
+            if self.__outputDevice is None:
+                self.open()
+            if self.__outputDevice is not None:
+                self.__outputDevice.lcd_display_string(text, line)
+        except:
+            logger.error('Showing text on lcd failed!')
+            self.close()
+
+
+# command line execution
+if __name__ == '__main__':
+    try:
+        display = LcdControl()
+        display.show('Duschzeit   5min', 1)
+        display.show('>>> Pruefe Karte', 2)
+        time.sleep(2.0)
+        display.show('>>> Karte OK <<<', 2)
+        time.sleep(2.0)
+        display.close()
+        display.show('Restart OK', 1)
+    except KeyboardInterrupt:
+        logger.debug('Key pressed - finishing now...')
+    except:
+        logger.error('Unknown error received. Test aborted!')
---
+++ tests/testlcdcontrol.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python
+
+import os
+import sys
+import unittest
+
+scriptPath = os.path.realpath(os.path.dirname(sys.argv[0]))
+os.chdir(scriptPath)
+sys.path.append("../")
+sys.path.append("../hd44780")
+
+from lcdcontrol import LcdControl
+from pishowerutils import logger
+
+# Control an I2C HD44780 LCD
+class TestLcdControl(unittest.TestCase):
+    display = None
+
+    def setUp(self):
+        self.assertIsNotNone(logger,            'logger object is not initialized!')
+
+        self.display = None
+        self.display = LcdControl()
+        self.assertIsNotNone(self.display,      'display object cannot be initialized!')
+        self.assertTrue(self.display.isOpen(),  'RFID display cannot be opened!')
+
+    def test_show(self):
+        self.assertIsNotNone(self.display,      'display object cannot be initialized!')
+
+        try:
+            logger.debug('displaying text...')
+            self.display.clear()
+            self.display.show('Hello World! #1', 1)
+            self.display.show('Hello World! #2', 2)
+        except:
+            self.fail('error during showing text!')
+
+    def test_reopen(self):
+        self.assertIsNotNone(self.display,      'display object cannot be initialized!')
+
+        self.display.close()
+        self.assertFalse(self.display.isOpen(), 'display cannot be closed!')
+
+        self.display.show('Reopened!', 2)
+        self.assertTrue(self.display.isOpen(),  'display cannot be reopened!')
+
+    def tearDown(self):
+        if self.display:
+            self.display.close()
+
+
+# exec tests
+if __name__ == "__main__":
+    unittest.main()
