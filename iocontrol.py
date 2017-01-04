8405e04c7bdb4bec5768aaf3a3e1c3b12a52a08b
3dh-de <3dh@3dh.de>
2017-01-04 01:37:08 +0100

    code beautified and adapted for new logger

--- iocontrol.py
+++ iocontrol.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python
 
+import time
+from pishowerutils import logger
+
 try:
     import RPi.GPIO as GPIO
 except RuntimeError:
-    print('Error importing RPi.GPIO! Superuser privileges needed!')
-
-import time
+    logger.error('Error importing RPi.GPIO! Superuser privileges needed!')
+    quit(-1)
 
 
 class RelayControl:
@@ -18,50 +20,50 @@ class RelayControl:
 
         for port in outports:
             GPIO.setup(port, GPIO.OUT)
-            print('init port #{}'.format(port))
+            logger.debug('init port #{}'.format(port))
         time.sleep(0.1)
 
         for port in outports:
             GPIO.output(port, GPIO.HIGH if self.invertedLevels == True else GPIO.LOW)
-            print('set port #{} to LOW'.format(port))
+            logger.debug('set port #{} to LOW'.format(port))
             time.sleep(0.01)
-        return
 
     def __del__(self):
         """ Clear GPIO interface """
         GPIO.cleanup()
-        return
 
     def toggleOutput(self, port, value=-1):
         if value is -1:
             value = GPIO.input(port)
-            print('toggle port from "{}"'.format(value))
+            logger.debug('toggle port from "{}"'.format(value))
             value = not value
-            print('toggle port to new "{}"'.format(value))
+            logger.debug('toggle port to new "{}"'.format(value))
         elif self.invertedLevels:
             value = not value
 
         state = GPIO.HIGH if value > 0 else GPIO.LOW
         GPIO.output(port, state)
-        print('toggle port #{0} to "{1}"'.format(port, state))
+        logger.debug('toggle port #{0} to "{1}"'.format(port, state))
         time.sleep(0.01)
-        return
 
 
-relay1 = 17                  # GPIO 17 / Pin 11 auf Ausgang setzen
-relay2 = 27                  # GPIO 27 / Pin 13 auf Ausgang setzen
+# command line execution
+if __name__ == '__main__':
+    relay1 = 17                  # GPIO 17 / Pin 11 auf Ausgang setzen
+    relay2 = 27                  # GPIO 27 / Pin 13 auf Ausgang setzen
 
-control = RelayControl([relay1, relay2])
-
-try:
-    time.sleep(2.0)
-    control.toggleOutput(relay1)
-    time.sleep(2.0)
-    control.toggleOutput(relay2)
-    time.sleep(2.0)
-    control.toggleOutput(relay2, False)
-    time.sleep(2.0)
-    control.toggleOutput(relay2, True)
-    time.sleep(2.0)
-except KeyboardInterrupt:
-    control = None
+    try:
+        control = RelayControl([relay1, relay2])
+        time.sleep(2.0)
+        control.toggleOutput(relay1)
+        time.sleep(2.0)
+        control.toggleOutput(relay2)
+        time.sleep(2.0)
+        control.toggleOutput(relay2, False)
+        time.sleep(2.0)
+        control.toggleOutput(relay2, True)
+        time.sleep(2.0)
+    except KeyboardInterrupt:
+        logger.debug('Key pressed - finishing now...')
+    except:
+        logger.error('Unknown error received. Reading aborted!')
