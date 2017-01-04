5e0b939418f34bb0963e4353b68213d202125d5f
3dh-de <3dh@3dh.de>
2017-01-04 01:27:23 +0100

    improved error handling and documentation

--- rfidreader.py
+++ rfidreader.py
@@ -1,13 +1,29 @@
 #!/usr/bin/env python3
 
+import glob
+import time
 from evdev import InputDevice, categorize, ecodes
 from pishowerutils import logger
 
 
 class UsbRfidReader:
-    """ Class for reading out RFID transponders via USB HID readers """
+    """
+    Class for reading out RFID transponders via USB HID readers
+
+    Opens USB HID devices like simple RFID readers for EM4100 and
+    Mifare NFC transponders and reads the received transponder
+    serial number.
+
+    Supported are Linux systems and any card/code readers registered
+    as HID devices under /dev/input/event[0..n].
+
+    WARNING:
+    File read permissions to the devices are needed - check your
+    user's groups! (e.g. for membership of group 'input')
+    """
 
     __inputDevice = None
+    __inputDevicePath = ''
     __deviceOpenRetries = 0
     __minCodeLength = 8
     __scancodes = {
@@ -23,10 +39,16 @@ class UsbRfidReader:
         53: u'/', 54: u'RSHFT', 56: u'LALT', 100: u'RALT'
     }
 
-    def __init__(self, inputEvent='/dev/input/event0', minCodeLength=8):
-        """ Init the input device object """
+    def __init__(self, devicePath='', minCodeLength=8):
+        """
+        Init the input device object
+
+        Parameter 'devicePath' for input event device like
+        '/dev/input/event0' is optional. If empty all available
+        input devices are tried.
+        """
         self.__minCodeLength = minCodeLength
-        self.__inputDevicePath = inputEvent
+        self.__inputDevicePath = devicePath
         self.open()
 
     def __del__(self):
@@ -34,31 +56,46 @@ class UsbRfidReader:
         self.close()
 
     def open(self):
-        """ Open the input device object for reading """
-        try:
-            self.close()
-            self.__inputDevice = InputDevice(self.__inputDevicePath)
-            self.__deviceOpenRetries = 0
-            time.sleep(1.0)     # prevent timing issues
-            logger.debug('Opened RFID reader device \'{0}\''.format(self.__inputDevicePath))
-        except:
-            if self.__deviceOpenRetries < 1:
-                logger.error('Unable to open USB RFID reader \'{0}\'!'.format(self.__inputDevicePath))
-            self.__inputDevice = None
-            self.__deviceOpenRetries += 1
+        """
+        Open an input event device for reading
+
+        Tries to open the device path given to the constructor
+        or search for all available input event devices.
+        """
+        devicePaths = []
+        if len(self.__inputDevicePath):
+            devicePaths = ["{0}".format(self.__inputDevicePath)]
+        else:
+            devicePaths = glob.glob('/dev/input/event*')  # search all available devices
+        for path in devicePaths:
+            try:
+                self.close()
+                self.__inputDevice = InputDevice(path)
+                self.__deviceOpenRetries = 0
+                self.__inputDevicePath = path
+                logger.debug('Opened RFID reader device \'{0}\''.format(path))
+                time.sleep(1.0)  # prevent timing issues
+                return
+            except:
+                if self.__deviceOpenRetries < 1:
+                    logger.error('Unable to open USB RFID reader \'{0}\'!'.format(path))
+                self.__inputDevice = None
+                self.__deviceOpenRetries += 1
 
     def close(self):
         """ Close the input device and wait 1 second """
         try:
-            if self.__inputDevice is None:
-                return
-            self.__inputDevice.close()
-            self.__inputDevice = None
-            time.sleep(1.0)     # prevent timing issues
+            if self.__inputDevice is not None:
+                self.__inputDevice.close()
+                self.__inputDevice = None
+                logger.debug('RFID input device closed')
+                time.sleep(1.0)  # prevent timing issues
         except:
             logger.warning('Error while closing RFID input device!')
-        else:
-            logger.debug('RFID input device closed')
+
+    def isOpen(self):
+        """ Returns True, if RFID reader is open """
+        return self.__inputDevice is not None
 
     def readCode(self):
         """ Try reading a code from the input device and return it as string """
@@ -82,7 +119,7 @@ class UsbRfidReader:
                     if data.scancode == 28:
                         # looking return key to be pressed
                         if len(code) < self.__minCodeLength:
-                            logger.warn('ignoring to small code: {0}'.format(code))
+                            logger.warning('ignoring to small code: {0}'.format(code))
                             break
                         else:
                             logger.debug('code read: \'{0}\''.format(code))
@@ -97,9 +134,6 @@ class UsbRfidReader:
 
 # command line execution
 if __name__ == '__main__':
-    import time
-    import sys
-
     rfidReader = UsbRfidReader('/dev/input/event2', 10)
     while True:
         try:
@@ -109,7 +143,7 @@ if __name__ == '__main__':
             time.sleep(0.5)
         except KeyboardInterrupt:
             logger.debug('Key pressed - finishing now...')
-            sys.exit(0)
+            break
         except:
             logger.error('Unknown error received. Reading aborted!')
-            time.sleep(2)
+            break
